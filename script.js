09f8c28b89c28afd11724a72104494329ea5eaa6
shantmelikyan <shantmelikyan@gmail.com>
2023-05-20 00:04:44 -0700

    slots implemented

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let myIndex = 0;
-carousel();
+// let myIndex = 0;
+// carousel();
 
-// image carousel slideshow
+// // image carousel slideshow
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  myIndex++;
-  if (myIndex > x.length) {
-    myIndex = 1;
-  }
-  x[myIndex - 1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {
+//     myIndex = 1;
+//   }
+//   x[myIndex - 1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
 
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
